bd82b4b79c75f5887da3032313c4b3eb5c4ea5cc
Luis <akade.luis@gmail.com>
2025-05-17 21:29:13 -0300

    init responsive fix

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
+function calcularPatrimonioDespachado() {
 
-function calcular() {
+	var num3 = document.getElementById('num3').value;
+	var num4 = document.getElementById('num4').value;
+
+	var resultadoIII = parseFloat(num3).toFixed(2) / parseInt(num4) - parseFloat(num3);
+	document.getElementById('resultadoIII').value = Math.abs(resultadoIII).toFixed(2).replace(".",",");
+}
+
+function calcularPatrimonioAtivo() {
+
+	var valor = document.querySelector("#valor").value;
+	var vida = document.querySelector("#vida").value;
+
+	var resultado = parseFloat(valor).toFixed(2) / parseInt(vida) - parseFloat(valor);
+
+	document.getElementById('resultado').value = Math.abs(resultado).toFixed(2).replace(".",",");
+};
+
+function calcularPatrimonioDesuso() {
 
 	var num1 = document.getElementById('num1').value;
 	var num2 = document.getElementById('num2').value;
